49e0625e276886d94afe8e444a38cd1719e2f642
Lu <1625196521@qq.com>
2020-09-14 22:32:51 +0800

    normal scores to count all scores

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -6,10 +6,11 @@ public class BowlingGame {
         for(int i = 0; i < scores.length - 1; ) {
             if (scores[i] == 10 && i < scores.length - 2) {
                 result += 10 + scores[i+1] + scores[i+2];
+                if (i == scores.length - 3) { break; }
                 i += 1;
             } else if (scores[i] < 10) {
                 if (scores[i] + scores[i+1] == 10) {
-                    result += scores[i] + scores[i+1] + scores[i+2];
+                    result += 10 + scores[i+2];
                 } else {
                     result += scores[i] + scores[i+1];
                 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -17,4 +17,18 @@ public class BowlingGameTest {
         int result = bowlingGame.score(scores);
         assertEquals(0,result);
     }
+
+    @Test
+    void normal_scores() {
+        BowlingGame bowlingGame = new BowlingGame();
+        int scores1[] = {2,3,0,5,7,1,2,2,0,0,0,4,1,3,4,1,7,1,2,2};
+        int scores2[] = {10,1,5,7,1,10,4,4,0,4,7,3,4,1,7,1,10,2,4};
+        int scores3[] = {2,3,0,5,7,1,2,2,1,0,10,4,1,3,4,1,7,3,7,8};
+        int result1 = bowlingGame.score(scores1);
+        int result2 = bowlingGame.score(scores2);
+        int result3 = bowlingGame.score(scores3);
+        assertEquals(47,result1);
+        assertEquals(103,result2);
+        assertEquals(76,result3);
+    }
 }
